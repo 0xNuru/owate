831afee6a44553caffa83c44875a57eeae75a81b
0xNuru <nurudeenmyself@yahoo.com>
2024-04-26 15:39:04 +0100

    add all services pages

--- js/main.js
+++ js/main.js
@@ -81,7 +81,7 @@ Version:	1.1
       loop: true,
       autoplay: true,
       smartSpeed: 500,
-      autoplayTimeout: 7000,
+      autoplayTimeout: 11000,
       singleItem: true,
       autoplayHoverPause: true,
       items: 1,
